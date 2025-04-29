727b35fbd3fbfb82af598c4d2d4a7b86d956c275
rahuldevsutradhar <rahulkumar016332@gmail.com>
2025-04-29 12:14:22 +0600

    add react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Page/Home';
+import Shop from './Page/Shop';
+import Product from './Page/Product';
+import About from './Page/About';
+import Contact from './Page/Contact';
+import Login from './Login';
+import Singup from './Singup';
+import Cart from './Page/Cart';
+import Checkout from './Page/Checkout';
+import Error from './Page/Error';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+    <Route path="/" element={<Home />}></Route>
+    <Route path="/shop" element={<Shop />}></Route>
+    <Route path="/product" element={<Product />}></Route>
+    <Route path="/about" element={<About />}></Route>
+    <Route path="/contact" element={<Contact />}></Route>
+    <Route path="/login" element={<Login />}></Route>
+    <Route path="/singup" element={<Singup />}></Route>
+    <Route path="/cart" element={<Cart />}></Route>
+    <Route path="/checkout" element={<Checkout />}></Route>
+    <Route path="/*" element={<Error />}></Route>
+    </>
+    
+  )
+);
+
 
 const App = () => {
   return (
-    <h1 className='bg-red-500'>telwaind install cheaking</h1>
+    <RouterProvider router={router} />
   )
 }
 
