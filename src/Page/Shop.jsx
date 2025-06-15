102ca3c457fefc86bf5cffe9f6cf02c83e0ac830
rahuldevsutradhar <rahulkumar016332@gmail.com>
2025-06-15 11:04:46 +0600

    add funsion shop page

--- src/Page/Shop.jsx
+++ src/Page/Shop.jsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom'
 import Pagination from './componants/Pagination'
 import Flex from './componants/Flex'
 import Container from './componants/Container'
+import Image from './componants/Image'
+import grid1 from '../assets/grid1.png'
+import grid2 from '../assets/grid2.png'
 
 
 const Shop = () => {
@@ -11,6 +14,11 @@ const Shop = () => {
   let data2 = useSelector((state) => state.bread.prevValue)
   let data3=useSelector((state)=>state.bread.aftervalue)
   
+  let [design, setDesign]=useState(true)
+  let handleDesign=()=>{
+   setDesign(!design)
+    
+  }
 
   return (
     <section>
@@ -35,7 +43,45 @@ const Shop = () => {
         <div className='w-3/12 '>Shop by Category</div>
         <div className='w-9/12 '>
 
-         <Pagination itemsPerPage={12}/>
+        <div className='flex justify-between items-center mb-[60px]'>
+          <Flex className=' gap-x-5'>
+            <div onClick={handleDesign}>
+                 <Image  src={grid1}/>
+            </div>
+            <div onClick={handleDesign}>
+                 <Image  src={grid2}/>
+            </div>
+          </Flex>
+
+        <Flex className='gap-x-15'>
+          <Flex className='gap-x-5 items-center'>
+          <p className='text-sm'>Sort by:</p>
+          <select name="" id="" className='border border-[#767676] py-2 px-5'>
+            <option value="">Featured</option>
+            <option value="">Red</option>
+            <option value="">Blue</option>
+            <option value="">Green</option>
+          </select>
+        </Flex>
+        <Flex className='gap-x-5 items-center'>
+          <p className='text-sm'>Show:</p>
+          <select name="" id="" className='border border-[#767676] py-2 px-5'>
+            <option value="6">6</option>
+            <option value="12">12</option>
+            <option value="24">24</option>
+          </select>
+        </Flex>
+        </Flex>
+        </div>
+
+        {
+          design 
+          ?
+            <Pagination itemsPerPage={12}/>
+          :
+            <h1>add new design</h1>
+        }
+
 
         </div>
 
